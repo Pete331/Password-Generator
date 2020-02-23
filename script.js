ef34b868572ae2f0dafd6c6c9e272ed1c3bd81d5
Peter Brennan <pweda64@hotmail.com>
2020-02-23 13:40:02 +0800

    split out character sets

--- script.js
+++ script.js
@@ -9,8 +9,39 @@ function generatePassword() {
     var length = prompt("Invalid: Password has to be between 8 and 128 characters in length. How Long would you like your password?");
   }
 
-  
-  (charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"),
+  // prompts for password character parameters and repeats if none selected
+  do {
+    alert("You must select at least one lowercase, uppercase, numeric or special characters")
+    var lowerCase = confirm("Would you like to include lowercase letters?");
+    var upperCase = confirm("Would you like to include uppercase letters?");
+    var numeric = confirm("Would you like to include numbers?");
+    var specialChar = confirm("Would you like to include special characters?");
+  }
+  while (lowerCase == false && upperCase == false && numeric == false && specialChar == false);
+
+  // defining the character sets
+  lowerCaseCharset = "abcdefghijklmnopqrstuvwxyz",
+  upperCaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numericCharset = "0123456789",
+  specialCharCharset = "`~!@#$%^&*()-_=+\|{}[]':;/?.,><";
+
+  // charset = lowerCaseCharset + upperCaseCharset + numericCharset + specialCharCharset;
+
+  var charset = ""
+// If lowercase and uppercase then add characterets together
+    if (lowerCase = 'true') {
+      charset += lowerCaseCharset;
+    }
+    if (upperCase = 'true') {
+      charset += upperCaseCharset;
+    }
+    if (numeric = 'true') {
+      charset += numericCharset;
+    }
+    if (specialChar = 'true') {
+      charset += specialCharCharset;
+    }
+
     (retVal = "");
   for (var i = 0, n = charset.length; i < length; ++i) {
     retVal += charset.charAt(Math.floor(Math.random() * n));
@@ -28,3 +59,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
